f8156054584d6e86d7c6df6c07031631498d2412
Dakas-1 <dakas9521@gmail.com>
2025-02-24 01:01:37 +0300

    added slug in routing

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -32,7 +32,7 @@ class MixController extends AbstractController
         ));
     }
 
-    #[Route('/mix/{id}', name: 'app_mix_show')]
+    #[Route('/mix/{slug}', name: 'app_mix_show')]
     public function show(?VinylMix $mix): Response
     {
         if (!$mix) {
@@ -58,7 +58,7 @@ class MixController extends AbstractController
         $this->addFlash('success', 'Vote counted!');
 
         return $this->redirectToRoute('app_mix_show', [
-            'id' => $mix->getId(),
+            'slug' => $mix->getSlug(),
         ]);
     }
 }
